d7da09ca7de33d366c01717d74d3f03be622d5bd
Lukasz <janouinteria@gmail.com>
2021-02-27 00:11:03 +0100

    changed urls. Multiple todos lists working

--- Todo/todo_app/urls.py
+++ Todo/todo_app/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, re_path
 from . import views
-from .views import addTodo, specific_todo_list, user_account, deleteCompleted, deleteAll
+from .views import addTodo, specific_todo_list, user_account, deleteCompleted, deleteAll, download_CSV
 
 urlpatterns = [
     # path('', views.log_in, name='login'),
@@ -10,7 +10,8 @@ urlpatterns = [
     path('account/', user_account, name='account'),
     re_path(r'addtodo/(?P<username>[\w-]+)/$', addTodo, name='addtodo'),
     re_path(r'^(?P<username>[\w-]+)/$', specific_todo_list, name='todolist'),
-    path('deletecomplete', deleteCompleted, name='deleteCompleted'),
-    path('deleteall', deleteAll, name='deleteall'),
+    re_path(r'deletecomplete/(?P<username>[\w-]+)/$', deleteCompleted, name='deleteCompleted'),
+    re_path(r'deleteall/(?P<username>[\w-]+)/$', deleteAll, name='deleteall'),
+    re_path(r'download_csv/(?P<username>[\w-]+)/$', download_CSV, name='download_csv'),
 
 ]
--- Todo/todo_app/views.py
+++ Todo/todo_app/views.py
@@ -13,6 +13,75 @@ from .decorators import redirect_authorised_user, redirect_notauthorised_user
 import csv
 
 
+def download_CSV(request, username):
+    response = HttpResponse(content='text/csv')
+
+    todo_name = request.GET.get('todo')
+    user = User.objects.get(id=request.user.id)
+    todo = Todo.objects.filter(user_id=user.id, todo_link=todo_name)
+
+    writer = csv.writer(response)
+    writer.writerow(['Todo', 'Todo_Description', 'Todo_link', 'Todo_added_date', 'Todo_completed'])
+    for todo in todo.values_list('text', 'description', 'link', 'date_added', 'complete'):
+        todo_elements = list(todo)
+        if todo_elements[2] == '//':
+            todo_elements[2] = ''
+        elif todo_elements[0] == '//':
+            todo_elements
+        writer.writerow(todo_elements)
+
+    response['Content-Disposition'] = 'attachment; filename="to_list.csv"'
+
+    return response
+
+
+def deleteAll(request, username):
+    todo_name = request.GET.get('todo')
+    user = User.objects.get(id=request.user.id)
+    todo = Todo.objects.filter(user_id=user.id, todo_link=todo_name).delete()
+
+    return redirect(request.META.get('HTTP_REFERER'))
+
+
+def deleteCompleted(request, username):
+    todo_name = request.GET.get('todo')
+    user = User.objects.get(id=request.user.id)
+    todo = Todo.objects.filter(complete__exact=True, user_id=user.id, todo_link=todo_name).delete()
+
+    return redirect(request.META.get('HTTP_REFERER'))
+
+
+def completeTodo(request, todo_id):
+    todo_name = request.GET.get('todo')
+    user = User.objects.get(id=request.user.id)
+    todo = Todo.objects.get(pk=todo_id, user_id=user.id)
+    todo.user_id = user.id
+    todo.complete = True
+    todo.save()
+
+    return redirect(request.META.get('HTTP_REFERER'))
+
+
+def addTodo(request, username):
+    todo_name = request.GET.get('todo')
+    form = Todo_list(request.POST)
+    user = User.objects.get(id=request.user.id)
+    if form.is_valid():
+        todo = Todo()
+        todo.text = request.POST['text']
+        todo.description = request.POST['description']
+        todo.link = request.POST['link']
+        todo.todo_link = todo_name
+        if 'https://www.' not in todo.link:
+            todo.link = '//' + todo.link
+        elif 'https://' not in todo.link:
+            todo.link = '//' + todo.link
+        todo.user_id = user.id
+        todo.save()
+
+    return redirect(request.META.get('HTTP_REFERER'))
+
+
 def specific_todo_list(request, username):
     todo_name = request.GET.get('todo')
     form = Todo_list
@@ -43,68 +112,6 @@ def user_account(request):
     # todo_list = todo_list.values_list('todo_link')
     content = {'todo_list': todo_list}
     return render(request, 'Todo/account.html', content)
-
-
-def download_CSV(request):
-    response = HttpResponse(content='text/csv')
-
-    writer = csv.writer(response)
-    writer.writerow(['Todo', 'Todo_Description', 'Todo_link', 'Todo_added_date', 'Todo_completed'])
-    for todo in Todo.objects.all().values_list('text', 'description', 'link', 'date_added', 'complete'):
-        todo_elements = list(todo)
-        if todo_elements[2] == '//':
-            todo_elements[2] = ''
-        elif todo_elements[0] == '//':
-            todo_elements
-        writer.writerow(todo_elements)
-
-    response['Content-Disposition'] = 'attachment; filename="to_list.csv"'
-
-    return response
-
-
-def deleteAll(request):
-    user = User.objects.get(id=request.user.id)
-    todo = Todo.objects.filter(user_id=user.id).delete()
-
-    return redirect(request.META.get('HTTP_REFERER'))
-
-
-def deleteCompleted(request):
-    user = User.objects.get(id=request.user.id)
-    todo = Todo.objects.filter(complete__exact=True, user_id=user.id).delete()
-
-    return redirect(request.META.get('HTTP_REFERER'))
-
-
-def completeTodo(request, todo_id):
-    user = User.objects.get(id=request.user.id)
-    todo = Todo.objects.get(pk=todo_id, user_id=user.id)
-    todo.user_id = user.id
-    todo.complete = True
-    todo.save()
-
-    return redirect(request.META.get('HTTP_REFERER'))
-
-
-def addTodo(request, username):
-    todo_name = request.GET.get('todo')
-    form = Todo_list(request.POST)
-    user = User.objects.get(id=request.user.id)
-    if form.is_valid():
-        todo = Todo()
-        todo.text = request.POST['text']
-        todo.description = request.POST['description']
-        todo.link = request.POST['link']
-        todo.todo_link = todo_name
-        if 'https://www.' not in todo.link:
-            todo.link = '//' + todo.link
-        elif 'https://' not in todo.link:
-            todo.link = '//' + todo.link
-        todo.user_id = user.id
-        todo.save()
-
-    return redirect(request.META.get('HTTP_REFERER'))
 
 
 @redirect_authorised_user
